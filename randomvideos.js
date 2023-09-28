8afafb923310543b2a4f85da014021d2e927229f
Nughazi Media <92838587+nughazimedia@users.noreply.github.com>
2023-09-29 05:35:46 +0700

    Update randomvideos.js
    Lebar Layar Video

--- randomvideos.js
+++ randomvideos.js
@@ -5,6 +5,6 @@ var videos = [
 ];
 
 var index=Math.floor(Math.random() * videos.length);
-var html='<iframe style="padding: 16px 0;height:250px;" src="https://www.youtube.com/embed/' + videos[index] + '" allowfullscreen></iframe>';
+var html='<iframe style="padding: 16px 0;width:100%;height:250px;" src="https://www.youtube.com/embed/' + videos[index] + '" allowfullscreen></iframe>';
 document.write(html);
 
