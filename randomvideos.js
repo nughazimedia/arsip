94f3fa1bf6978457309e823b95a0a3a516ff8366
Nughazi Media <92838587+nughazimedia@users.noreply.github.com>
2023-09-29 05:18:05 +0700

    Add files via upload

---
+++ randomvideos.js
@@ -0,0 +1,11 @@
+var videos = [
+'7WTmlq4FmlY',
+'GPp7ykvdxVI',
+'89fPcUyAm4M',
+'JgqLZNWXI5g'
+];
+
+var index=Math.floor(Math.random() * videos.length);
+var html='<iframe style="padding: 16px 0;height:250px;" src="https://www.youtube.com/embed/' + videos[index] + '" allowfullscreen></iframe>';
+document.write(html);
+
